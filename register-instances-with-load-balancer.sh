6ca6e626f73bb2d8345d2e59530b595b3def9b76
tkit <unchemist@gmail.com>
2016-09-17 15:05:29 +0900

    first commit

---
+++ register-instances-with-load-balancer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# $1 : SIDE ; blue/green
+# $2 : LB Name
+
+# blue / green
+SIDE=$1
+# LB Name
+LB_NAME=$2
+
+INSTANCES=$(aws ec2 describe-instances --filters "Name=tag:ServerType,Values=web" "Name=tag:Side,Values=${SIDE}" --query "Reservations[].Instances[].[InstanceId]" --output text)
+
+for i in `echo ${INSTANCES}` ;do
+  aws elb register-instances-with-load-balancer --load-balancer-name ${LB_NAME} --instances $i
+done
